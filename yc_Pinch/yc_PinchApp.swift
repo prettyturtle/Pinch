e010c2d2b1c2cec364242ea61bd44cd76a01cc2f
prettyturtle <unifconti@gmail.com>
2023-08-14 00:37:59 +0900

    Initial Commit

---
+++ yc_Pinch/yc_PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  yc_PinchApp.swift
+//  yc_Pinch
+//
+//  Created by yc on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct yc_PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
