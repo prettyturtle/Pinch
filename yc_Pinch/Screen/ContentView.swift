ebed89ee27b8d8051d9dfc867714ddf320a5a557
prettyturtle <unifconti@gmail.com>
2023-08-14 01:07:51 +0900

    Double Tap Scale Up

--- yc_Pinch/Screen/ContentView.swift
+++ yc_Pinch/Screen/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     // MARK: - PROPERTY
     
     @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
     
     // MARK: - FUNCTION
     
@@ -31,6 +32,16 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .scaleEffect(imageScale)
+                    .animation(.spring(), value: imageScale)
+                // MARK: - 1. TAP GESTURE
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            imageScale = 5
+                        } else {
+                            imageScale = 1
+                        }
+                    }
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
