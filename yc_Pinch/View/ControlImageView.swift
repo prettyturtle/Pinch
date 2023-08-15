aabb6718fdd1ae9bb298442cf4e47c82eccb2730
prettyturtle <unifconti@gmail.com>
2023-08-16 00:39:49 +0900

    Magnify Control

---
+++ yc_Pinch/View/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  yc_Pinch
+//
+//  Created by yc on 2023/08/16.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
